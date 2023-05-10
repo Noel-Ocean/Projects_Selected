30061ca49a974155033112efa9c20c20af717ab5
Noel-Ocean <noel.leuangyifun@yahoo.com>
2023-05-10 20:16:29 +0800

    update

--- Frog Story Corpora/Analysis.py
+++ Frog Story Corpora/Analysis.py
@@ -144,23 +144,24 @@ def q2_sentence_cleaning (df_dample, spa_or_eng_model):
     5. For each set of word tokens, remove the word with less than 2 letters
     6. For each set of cleaned word tokens, concatenate back to sentences
     '''
-
-    stopwords = spa_or_eng_model.Defaults.stop_words
+    
     symbols = [',', '.', '!', '?', ':']
+    stopwords_list = spa_or_eng_model.Defaults.stop_words
 
     paragraphs_to_sentences = df_dample.text.apply(lambda x: x.split("| "))
     sentence_list = list(np.concatenate(paragraphs_to_sentences).flat)
     word_token = [word_tokenize(i) for i in sentence_list]
-    word_token_clean = list(map(lambda x: [i for i in x if not i in stopwords], word_token))
+    word_token_clean = list(map(lambda x: [i for i in x if not i in stopwords_list], word_token))
     word_token_clean = list(map(lambda x: [i for i in x if not i in symbols], word_token_clean))
     word_token_clean = [i for i in word_token_clean if i!= []]
 
     cleaned_words_to_sentences = [" ".join(i) for i in word_token_clean]
-
+    
     print("Preview: cleaned sentences")
     print(cleaned_words_to_sentences[1:5])
 
     return cleaned_words_to_sentences
+
 
 def q2_part_of_speech (cleaned_sentences_list, spa_or_eng_model):
     '''
@@ -190,33 +191,6 @@ def q2_part_of_speech (cleaned_sentences_list, spa_or_eng_model):
     
     output = [pos_group_dict, pos_count_dict]
     return output
-
-# ---------------------------------------------------------------------------- #
-#           Function 5: RQ3: Usage frequenies of interested keywords           #
-# ---------------------------------------------------------------------------- #
-def q3_keyword_frequency(cleaned_sentences_list, keywords_list):
-        '''
-        Function - keywords matching & counting for Research Question 2
-        1. Split the cleaned sentences into words
-        2. Transform each interested keyword to lower case
-        3. For each keyword, count the occurences
-        4. Transform the counting into readable dataframes
-        5. Result = 4
-        '''
-            
-        sentences_to_words = [word_tokenize(i) for i in cleaned_sentences_list]
-        sentences_to_words = list(np.concatenate(sentences_to_words).flat)
-
-        keywords_lower = [i.lower() for i in keywords_list]
-        keywords_count = [sentences_to_words.count(i) for i in keywords_lower]
-        keywords_dict = dict(zip(keywords_lower, keywords_count))
-
-        output = pd.DataFrame(list(keywords_dict.items()))
-        output.columns = ["keyword", "count"]
-
-        return output
-
-
 
 
 
